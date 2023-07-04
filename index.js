f4de44ddb3ca4380b4c35324ea9ca64b314194de
MehmetCanWT <52177084+MehmetCanWT@users.noreply.github.com>
2023-07-04 19:49:34 +0300

    anime

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-fetch('https://api.nekosapi.com/v2/images/random')
-  .then(res => res.json())
-  .then((res) => {
-    console.log(res.data.attributes.file)
+function get_random_anime(){
+
+  url = "https://api.nekosapi.com/v2/images/random";
+
+  fetch(url)
+  .then(function(response){
+    return response.json();
   })
+  .then(function(data){
+    display_image(data.data.attributes.file);
+  })
+  .catch(function(error){
+    console.log("Error: " + error);
+  });
+
+
+}
+
+function display_image(image_url){
+  document.getElementById("image").src = image_url;
+}
