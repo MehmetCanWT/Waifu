bd4e5a4e84209a739c4f777026a6e84640a06c76
MehmetCanWT <52177084+MehmetCanWT@users.noreply.github.com>
2023-07-06 21:54:59 +0300

    categories

--- css and js/sfw.js
+++ css and js/sfw.js
@@ -22,6 +22,9 @@ function get_waifu(){
   }
 
 
+
+
+
   function get_neko(){
 
     url = "https://api.waifu.pics/sfw/neko";
@@ -44,7 +47,223 @@ function get_waifu(){
   function display_image(image_url){
     document.getElementById("image").src = image_url;
   }
+
+
+
   
+
+  function get_cuddle(){
+
+    url = "https://api.waifu.pics/sfw/cuddle";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_kiss(){
+
+    url = "https://api.waifu.pics/sfw/kiss";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_lick(){
+
+    url = "https://api.waifu.pics/sfw/lick";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_blush(){
+
+    url = "https://api.waifu.pics/sfw/blush";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_bite(){
+
+    url = "https://api.waifu.pics/sfw/bite";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_bully(){
+
+    url = "https://api.waifu.pics/sfw/bully";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+  
+
+  function get_poke(){
+
+    url = "https://api.waifu.pics/sfw/poke";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+
+
+
+
+  
+
+  function get_dance(){
+
+    url = "https://api.waifu.pics/sfw/dance";
+  
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      display_image(data.url);
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    });
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
   
   
   
