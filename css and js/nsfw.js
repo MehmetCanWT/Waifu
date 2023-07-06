31d66f4e76b6ebdd3deea1bac3082b863cc87b41
MehmetCanWT <52177084+MehmetCanWT@users.noreply.github.com>
2023-07-06 21:40:34 +0300

    a

--- css and js/nsfw.js
+++ css and js/nsfw.js
@@ -49,3 +49,59 @@ function display_image(image_url){
 
 
 
+
+
+
+function get_trap(){
+
+  url = "https://api.waifu.pics/nsfw/trap";
+
+  fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    display_image(data.url);
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log("Error: " + error);
+  });
+
+
+}
+
+function display_image(image_url){
+  document.getElementById("image").src = image_url;
+}
+
+
+
+
+
+
+function get_blowjob(){
+
+  url = "https://api.waifu.pics/nsfw/blowjob";
+
+  fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    display_image(data.url);
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log("Error: " + error);
+  });
+
+
+}
+
+function display_image(image_url){
+  document.getElementById("image").src = image_url;
+}
+
+
+
